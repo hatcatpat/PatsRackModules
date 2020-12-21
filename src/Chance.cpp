962d350ae65d9a3d196fea78cedf4734dd8795aa
Pat Caulfield <patrickcaulfield1177@gmail.com>
2020-12-21 23:11:58 +0000

    first commit
    hi mum

---
+++ src/Chance.cpp
@@ -0,0 +1,85 @@
+#include "plugin.hpp"
+
+struct Chance : Module
+{
+	enum ParamIds
+	{
+		INPUT1_PARAM,
+		INPUT2_PARAM,
+		INPUT3_PARAM,
+		INPUT4_PARAM,
+		NUM_PARAMS
+	};
+	enum InputIds
+	{
+		GATE_INPUT,
+		CV_1_INPUT,
+		CV_2_INPUT,
+		CV_3_INPUT,
+		CV_4_INPUT,
+		NUM_INPUTS
+	};
+	enum OutputIds
+	{
+		OUTPUT1_OUTPUT,
+		OUTPUT2_OUTPUT,
+		OUTPUT3_OUTPUT,
+		OUTPUT4_OUTPUT,
+		NUM_OUTPUTS
+	};
+	enum LightIds
+	{
+		NUM_LIGHTS
+	};
+
+	int open[4] = {0};
+	dsp::SchmittTrigger trigger;
+
+	Chance()
+	{
+		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+		configParam(INPUT1_PARAM, 0.f, 1.f, 0.5f, "Chance For Output 1");
+		configParam(INPUT2_PARAM, 0.f, 1.f, 0.5f, "Chance For Output 2");
+		configParam(INPUT3_PARAM, 0.f, 1.f, 0.5f, "Chance For Output 3");
+		configParam(INPUT4_PARAM, 0.f, 1.f, 0.5f, "Chance For Output 4");
+	}
+
+	void process(const ProcessArgs &args) override
+	{
+		
+		if (trigger.process(rescale(inputs[GATE_INPUT].getVoltage(), 0.1f, 2.0f, 0.0f, 1.0f)))
+		{
+			for(int i = 0; i < 4; ++i){
+
+			}
+		}
+
+	}
+};
+
+struct ChanceWidget : ModuleWidget
+{
+	ChanceWidget(Chance *module)
+	{
+		setModule(module);
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Chance.svg")));
+
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(5.08, 20.078)), module, Chance::INPUT1_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(5.08, 48.187)), module, Chance::INPUT2_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(5.08, 76.297)), module, Chance::INPUT3_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(5.08, 104.406)), module, Chance::INPUT4_PARAM));
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 12.047)), module, Chance::GATE_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 20.078)), module, Chance::CV_1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 48.187)), module, Chance::CV_2_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 76.297)), module, Chance::CV_3_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 104.406)), module, Chance::CV_4_INPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 32.125)), module, Chance::OUTPUT1_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 60.234)), module, Chance::OUTPUT2_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 88.344)), module, Chance::OUTPUT3_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 116.453)), module, Chance::OUTPUT4_OUTPUT));
+	}
+};
+
+Model *modelChance = createModel<Chance, ChanceWidget>("Chance");
---
+++ src/plugin.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <rack.hpp>
+
+
+using namespace rack;
+
+// Declare the Plugin, defined in plugin.cpp
+extern Plugin* pluginInstance;
+
+extern Model* modelTom;
+extern Model* modelChance;
+
+// Declare each Model, defined in each module source file
+// extern Model* modelMyModule;
